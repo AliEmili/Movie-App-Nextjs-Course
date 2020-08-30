9a5732e0e918fc6afc7fa428949858d84f13d017
aliemili <aliemili2013@yahoo.com>
2020-08-30 21:04:32 +0430

    Different kinds of components and the explanation when to use each

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+// ------ Arrow Function -----
+// WHEN TO USE: 
+// 1.for smaller components
+// 2.reusabl components
+// 3.presentational components, partially 
+
+// const About = () => (
+//     <h1>Hello About Page</h1>
+// )
+
+
+
+
+// Normal Function
+
+// function About(){
+//     return(
+//         <h1>Hello About Page</h1>
+//     )
+// }
+
+
+// class About extends React.Component{
+//     render(){
+//         return(
+//             <h1>hello</h1>
+//         )
+//     }
+    
+// }
+export default About;
