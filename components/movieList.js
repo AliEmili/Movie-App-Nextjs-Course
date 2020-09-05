6120a64d42cd04aacb5e11a1a19ae98537d0c330
aliemili <aliemili2013@yahoo.com>
2020-09-05 20:19:56 +0430

    store data in seperate js file

--- components/movieList.js
+++ components/movieList.js
@@ -1,16 +1,23 @@
 class movieList extends React.Component{
+
+    shorten = (text, maxLength)=> {
+        if(text && text.length > maxLength){
+            return text.substr(0,maxLength) + '...';    
+        }
+        return text
+    }
     renderMovies(movies){
         
         return movies.map((movie)=>
             (
-                <div className="col-lg-4 col-md-6 mb-4">
+                <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
                     <div className="card h-100">
                     <a href="#"><img className="card-img-top" src={movie.image} alt="" /></a>
                     <div className="card-body">
                         <h4 className="card-title">
                         <a href="#">{movie.name}</a>
                         </h4>
-                        <p className="card-text">{movie.description}</p>
+                        <p className="card-text">{this.shorten(movie.description, 100)}</p>
                     </div>
                     <div className="card-footer">
                         <small className="text-muted">{movie.rating}</small>
