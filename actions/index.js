641829e3b55bcbeed85c565ea9e01815474ce35f
aliemili <aliemili2013@yahoo.com>
2020-10-01 17:36:30 +0330

    get movie by id

--- actions/index.js
+++ actions/index.js
@@ -41,15 +41,17 @@ export const createMovie = (movie) => {
 }
 
 export const getMovieById = (id) => {
-    return new Promise((resolve, reject) => {
-        const movieIndex = MOVIE_DATA.findIndex((movie) => {
-            return movie.id === id;
-        })
-        const movie = MOVIE_DATA[movieIndex];
-        setTimeout(() => {
-            resolve(movie);
-        }, 50);
-    })
+
+    return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data);
+    // return new Promise((resolve, reject) => {
+    //     const movieIndex = MOVIE_DATA.findIndex((movie) => {
+    //         return movie.id === id;
+    //     })
+    //     const movie = MOVIE_DATA[movieIndex];
+    //     setTimeout(() => {
+    //         resolve(movie);
+    //     }, 50);
+    // })
 }
 
 export const getCategories = () => {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const next = require('next');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,13 +11,31 @@ app.prepare().then(() => {
 
     const server = express();
 
-    server.get('/api/v1/movies', (req,res)=> {
-        return res.json(moviesData);
-    })
 
-    server.get('*', (req, res) => {
-        return handle(req, res);
-    })
+    server.get('/api/v1/movies', (req, res) => {
+        return res.json(moviesData);
+    });
+
+    server.get('/api/v1/movies/:id', (req, res) => {
+        const { id } = req.params;
+
+        const movie = moviesData.find(m => m.id === id);
+
+        return res.json(movie);
+
+    });
+
+    server.post('/api/v1/movies', (req, res) => {
+        const movie = req.body;
+        console.log(JSON.stringify(movie));
+        return res.json({...movie, createdTime: 'today', author: 'me' });
+    });
+
+    server.delete('api/v1/movies/:id', (req, res) => {
+        const { id } = req.params;
+        return res.json({ message: `Deleting post of id ${id}` });
+    });
+
 
     const PORT = process.env.PORT || 3000;
 
